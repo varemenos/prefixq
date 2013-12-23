d42b7fa9b0372c03bbdfdb7d54d75f1795e97359
Adonis K <aklp08@gmail.com>
2013-12-23 13:34:50 +0200

    included express in the project, generate css properties via function

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 var fs = require('fs');
-var prefix = 'css/properties/';
+var express = require('express');
 
-var data = fs.readFileSync('css-properties.json', {'encoding': 'utf8'});
-data = JSON.parse(data).query.results;
-var properties = [];
+var getCssProperties = function () {
+	var prefix = 'css/properties/';
 
-for (property in data) {
-	if (data.hasOwnProperty(property)) {
-		property = property.replace(prefix, '');
+	var data = fs.readFileSync('css-properties.json', {'encoding': 'utf8'});
+	data = JSON.parse(data).query.results;
+	var properties = [];
 
-		if (property.substring(0, 3) !== 'ms-') {
-			properties.push(property);
+	for (property in data) {
+		if (data.hasOwnProperty(property)) {
+			property = property.replace(prefix, '');
+
+			if (property.substring(0, 3) !== 'ms-') {
+				properties.push(property);
+			}
 		}
 	}
+
+	return properties;
 }
+
+var properties = getCssProperties();
 
 console.log(properties);
